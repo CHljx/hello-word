df8018b8cd0296417884e9d6070010c1cd649487
lijiaxin@37.com <lijiaxin@37.com>
2018-10-21 22:48:29 +0800

    小游戏测试

---
+++ src/settings.13f0e.js
@@ -0,0 +1,90 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    rawAssets: {
+        assets: {}
+    },
+    launchScene: "db://assets/scene/index.fire",
+    scenes: [
+        {
+            url: "db://assets/scene/index.fire",
+            uuid: "aeox6S2alFtYAq1hQKz8vK"
+        },
+        {
+            url: "db://assets/scene/game.fire",
+            uuid: "c5qoTxGWdLtIdLnu30JawN"
+        }
+    ],
+    packedAssets: {
+        "07c204bc1": [
+            "066Ra4NY9BOZ2HzX6CSHEG",
+            "28duU/sFNJTYkZjfdfP6a8",
+            "29FYIk+N1GYaeWH/q1NxQO",
+            "325EsIPL5EQq0+yyxESQxI",
+            "48gAt06vFDlqtLJE8rrO9H",
+            "55j1/2gsdIso7DhOcrQ1Um",
+            "675ovJ2tVK2aLYfgPUWOMv",
+            "871mXLK29Ly7SZ/cPuLdtI",
+            "8855/VlrRKd6H0MSRnFxAU",
+            "9bvaMerUlDyary99mJa6xp",
+            "c5qoTxGWdLtIdLnu30JawN",
+            "cchTOs/PVMOqt5g0qRoxb8",
+            "e97GVMl6JHh5Ml5qEDdSGa"
+        ],
+        "09e4c8e13": [
+            "18BR0xGftKpYm/M9vQsRYA",
+            "28duU/sFNJTYkZjfdfP6a8",
+            "6eayHhVrJIfqfFOgUG8DGn",
+            "9bvaMerUlDyary99mJa6xp",
+            "a9biALAQdNsrpRPllPGsxD",
+            "aeox6S2alFtYAq1hQKz8vK",
+            "b4mO6NCmVDFZaOvOydiCHr",
+            "fcbggg4aVP57RolQHDsosC"
+        ],
+        "0b7bbfcb1": [
+            "2fS7N3qZRINqjPl9CwH+1W",
+            "33q/WC/XVDcavz1Q5mB1Np",
+            "71VhFCTINJM6/Ky3oX9nBT",
+            "75hi4U80pGQan2OAVsZiCj",
+            "99FwsL0hBG8bITfZ4/IwmK",
+            "b23KZr/qZJJpPDhPUz5H8X",
+            "b4P/PCArtIdIH38t6mlw8Y",
+            "bbMSO4vSRHoJkEqM0kG8cB",
+            "cf73jxyN9Jt47QTJU6ziYh",
+            "d69d5m8VRC5q7CS0OGJGJW",
+            "d8HsitJHxOYqo801xBk8ev",
+            "d9Mj0jwAhEmpnrHGSUQEF5",
+            "ed/mS53t5Pp4x+ET6dtssm",
+            "efSBqoxG1PcaIpFPVYvl99"
+        ]
+    },
+    orientation: "portrait",
+    debug: true,
+    subpackages: {},
+    md5AssetsMap: {
+        "07/07c204bc1.json": "f3dcb",
+        "09/09e4c8e13.json": "08e62",
+        "0b/0b7bbfcb1.json": "c7246",
+        "assets/2f/2f4bb377-a994-4836-a8cf-97d0b01fed56.jpg": "4e9b7",
+        "assets/33/33abf582-fd75-4371-abf3-d50e66075369.jpg": "610d6",
+        "assets/71/71561142-4c83-4933-afca-cb7a17f67053.png": "286c6",
+        "assets/75/75862e14-f34a-4641-a9f6-38056c6620a3.png": "29e0a",
+        "assets/99/99170b0b-d210-46f1-b213-7d9e3f23098a.png": "9cdfa",
+        "assets/b2/b2dca66b-fea6-4926-93c3-84f533e47f17.png": "7f1de",
+        "assets/b4/b43ff3c2-02bb-4874-81f7-f2dea6970f18.png": "bedf4",
+        "assets/bb/bb3123b8-bd24-47a0-9904-a8cd241bc701.jpg": "ae682",
+        "assets/cf/cfef78f1-c8df-49b7-8ed0-4c953ace2621.png": "69ff9",
+        "assets/d6/d6f5de66-f154-42e6-aec2-4b4386246256.jpg": "debce",
+        "assets/d8/d81ec8ad-247c-4e62-aa3c-d35c4193c7af.png": "cdbc9",
+        "assets/d9/d9323d23-c008-449a-99eb-1c6494404179.png": "066fa",
+        "assets/ed/edfe64b9-dede-4fa7-8c7e-113e9db6cb26.png": "db099",
+        "assets/ef/ef481aa8-c46d-4f71-a229-14f558be5f7d.png": "4309a"
+    }
+};
