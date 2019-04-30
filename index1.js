6526047f49e281a0170d96e6d7b90d88554e5081
lijiaxin@37.com <lijiaxin@37.com>
2019-04-30 15:08:00 +0800

    test

--- index1.js
+++ index1.js
@@ -1,9 +1,6 @@
 (function (cjs, an) {
 var lib={};
-(lib.爆装备_0 = function() {
-	this.initialize(img.爆装备_0);
-}).prototype = p = new cjs.Bitmap();
-p.nominalBounds = new cjs.Rectangle(0,0,580,435);
+
 })(createjs = createjs||{}, AdobeAn = AdobeAn||{});
 var createjs, AdobeAn;
 
