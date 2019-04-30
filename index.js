26903706f6c471a762a0882724e3ca64114de631
lijiaxin@37.com <lijiaxin@37.com>
2019-04-30 10:29:53 +0800

    test

--- index.js
+++ index.js
@@ -1,5 +1,238 @@
 (function (cjs, an) {
 
+var p; // shortcut to reference prototypes
+var lib={};var ss={};var img={};
+lib.ssMetadata = [];
+
+
+// symbols:
+
+// library properties:
+lib.properties = {
+	id: '6B0F5B5223810D41B7F6ED92E9D2DD73',
+	width: 750,
+	height: 1334,
+	fps: 60,
+	color: "#FFFFFF",
+	opacity: 1.00,
+	manifest: [
+		{src:"images/爆装备_0.png", id:"爆装备_0"},
+		{src:"images/爆装备_10.png", id:"爆装备_10"},
+		{src:"images/爆装备_2.png", id:"爆装备_2"},
+		{src:"images/爆装备_4.png", id:"爆装备_4"},
+		{src:"images/爆装备_6.png", id:"爆装备_6"},
+		{src:"images/爆装备_8.png", id:"爆装备_8"},
+		{src:"images/合成1_0.png", id:"合成1_0"},
+		{src:"images/合成1_1复制.png", id:"合成1_1复制"},
+		{src:"images/合成1_2.png", id:"合成1_2"},
+		{src:"images/合成1_3.png", id:"合成1_3"},
+		{src:"images/合成1_4.png", id:"合成1_4"},
+		{src:"images/合成1_5.png", id:"合成1_5"},
+		{src:"images/合成1_6.png", id:"合成1_6"},
+		{src:"images/合成1_7.png", id:"合成1_7"},
+		{src:"images/戒指_.png", id:"戒指"},
+		{src:"images/_00024png复制2.png", id:"_00024png复制2"},
+		{src:"images/_00025PNG复制2.png", id:"_00025PNG复制2"},
+		{src:"images/_00025png复制3.png", id:"_00025png复制3"},
+		{src:"images/_00026png复制2.png", id:"_00026png复制2"},
+		{src:"images/_00027PNG复制2.png", id:"_00027PNG复制2"},
+		{src:"images/_00027png复制3.png", id:"_00027png复制3"},
+		{src:"images/_00040.png", id:"_00040"},
+		{src:"images/_00041.png", id:"_00041"},
+		{src:"images/_00042.png", id:"_00042"},
+		{src:"images/_00043.png", id:"_00043"},
+		{src:"images/_00050.png", id:"_00050"},
+		{src:"images/_00051.png", id:"_00051"},
+		{src:"images/_00052.png", id:"_00052"},
+		{src:"images/_00053.png", id:"_00053"},
+		{src:"images/_00136.png", id:"_00136"},
+		{src:"images/_00137.png", id:"_00137"},
+		{src:"images/_00138.png", id:"_00138"},
+		{src:"images/_00139.png", id:"_00139"},
+		{src:"images/_00140.png", id:"_00140"},
+		{src:"images/_00390.png", id:"_00390"},
+		{src:"images/_00391.png", id:"_00391"},
+		{src:"images/_00392.png", id:"_00392"},
+		{src:"images/_00393.png", id:"_00393"},
+		{src:"images/_00400.png", id:"_00400"},
+		{src:"images/_00401.png", id:"_00401"},
+		{src:"images/_00402.png", id:"_00402"},
+		{src:"images/_00403.png", id:"_00403"},
+		{src:"images/_00410.png", id:"_00410"},
+		{src:"images/_00411.png", id:"_00411"},
+		{src:"images/_00412.png", id:"_00412"},
+		{src:"images/_00413.png", id:"_00413"},
+		{src:"images/_00416.png", id:"_00416"},
+		{src:"images/_00417.png", id:"_00417"},
+		{src:"images/_00418.png", id:"_00418"},
+		{src:"images/_00419.png", id:"_00419"},
+		{src:"images/_00420.png", id:"_00420"},
+		{src:"images/_00421.png", id:"_00421"},
+		{src:"images/_01224.png", id:"_01224"},
+		{src:"images/_01225.png", id:"_01225"},
+		{src:"images/_03624.png", id:"_03624"},
+		{src:"images/_03626.png", id:"_03626"},
+		{src:"images/_04016.png", id:"_04016"},
+		{src:"images/_04017.png", id:"_04017"},
+		{src:"images/_04018.png", id:"_04018"},
+		{src:"images/_04019.png", id:"_04019"},
+		{src:"images/_04020.png", id:"_04020"},
+		{src:"images/_04021.png", id:"_04021"},
+		{src:"images/_0b2814dec5f4e8789429ada1a88c833929e76e642983YZOSNK.jpg", id:"_0b2814dec5f4e8789429ada1a88c833929e76e642983YZOSNK"},
+		{src:"images/_17.jpg", id:"_17"},
+		{src:"images/_19.jpg", id:"_19"},
+		{src:"images/_1000.png", id:"_1000"},
+		{src:"images/_10016.png", id:"_10016"},
+		{src:"images/_10019.png", id:"_10019"},
+		{src:"images/_10144.png", id:"_10144"},
+		{src:"images/_10145.png", id:"_10145"},
+		{src:"images/_10146.png", id:"_10146"},
+		{src:"images/_10147.png", id:"_10147"},
+		{src:"images/_10148.png", id:"_10148"},
+		{src:"images/_10149.png", id:"_10149"},
+		{src:"images/_10824.png", id:"_10824"},
+		{src:"images/_10826.png", id:"_10826"},
+		{src:"images/_5000.png", id:"_5000"},
+		{src:"images/_91.jpg", id:"_91"},
+		{src:"images/_92.jpg", id:"_92"},
+		{src:"images/_93.jpg", id:"_93"},
+		{src:"images/祝福_.png", id:"祝福"},
+		{src:"images/祝福油_.png", id:"祝福油"},
+		{src:"images/biao.png", id:"biao"},
+		{src:"images/boss.png", id:"boss"},
+		{src:"images/chedi.png", id:"chedi"},
+		{src:"images/元宝_.png", id:"元宝"},
+		{src:"images/di.png", id:"di"},
+		{src:"images/dion.png", id:"dion"},
+		{src:"images/ditu.jpg", id:"ditu"},
+		{src:"images/du.png", id:"du"},
+		{src:"images/guaji.png", id:"guaji"},
+		{src:"images/guan.png", id:"guan"},
+		{src:"images/升级2_10.png", id:"升级2_10"},
+		{src:"images/升级2_11.png", id:"升级2_11"},
+		{src:"images/升级2_12.png", id:"升级2_12"},
+		{src:"images/升级2_13.png", id:"升级2_13"},
+		{src:"images/升级2_2.png", id:"升级2_2"},
+		{src:"images/升级2_3.png", id:"升级2_3"},
+		{src:"images/升级2_4.png", id:"升级2_4"},
+		{src:"images/升级2_5.png", id:"升级2_5"},
+		{src:"images/升级2_6.png", id:"升级2_6"},
+		{src:"images/升级2_7.png", id:"升级2_7"},
+		{src:"images/升级2_8.png", id:"升级2_8"},
+		{src:"images/升级2_9.png", id:"升级2_9"},
+		{src:"images/li.png", id:"li"},
+		{src:"images/shiqu.png", id:"shiqu"},
+		{src:"images/tou.png", id:"tou"},
+		{src:"images/zhi.png", id:"zhi"},
+		{src:"images/彻地钉_0.png", id:"彻地钉_0"},
+		{src:"images/彻地钉_1.png", id:"彻地钉_1"},
+		{src:"images/彻地钉_2.png", id:"彻地钉_2"},
+		{src:"images/彻地钉_3.png", id:"彻地钉_3"},
+		{src:"images/彻地钉_4.png", id:"彻地钉_4"},
+		{src:"images/彻地钉_5.png", id:"彻地钉_5"},
+		{src:"images/技能书_.png", id:"技能书"},
+		{src:"images/任务完成_.png", id:"任务完成"},
+		{src:"images/金条_.png", id:"金条"},
+		{src:"images/闪电_0.png", id:"闪电_0"},
+		{src:"images/闪电_2.png", id:"闪电_2"},
+		{src:"images/闪电_4.png", id:"闪电_4"},
+		{src:"sounds/传奇经典音乐_.mp3", id:"传奇经典音乐"},
+		{src:"sounds/男攻击_.mp3", id:"男攻击"}
+	],
+	preloads: []
+};
+
+
+
+// bootstrap callback support:
+
+
+p.setAutoPlay = function(autoPlay) {
+	this.tickEnabled = autoPlay;
+}
+p.play = function() { this.tickEnabled = true; this.getChildAt(0).gotoAndPlay(this.getTimelinePosition()) }
+p.stop = function(ms) { if(ms) this.seek(ms); this.tickEnabled = false; }
+p.seek = function(ms) { this.tickEnabled = true; this.getChildAt(0).gotoAndStop(lib.properties.fps * ms / 1000); }
+p.getDuration = function() { return this.getChildAt(0).totalFrames / lib.properties.fps * 1000; }
+
+p.getTimelinePosition = function() { return this.getChildAt(0).currentFrame / lib.properties.fps * 1000; }
+
+an.bootcompsLoaded = an.bootcompsLoaded || [];
+if(!an.bootstrapListeners) {
+	an.bootstrapListeners=[];
+}
+
+an.bootstrapCallback=function(fnCallback) {
+	an.bootstrapListeners.push(fnCallback);
+	if(an.bootcompsLoaded.length > 0) {
+		for(var i=0; i<an.bootcompsLoaded.length; ++i) {
+			fnCallback(an.bootcompsLoaded[i]);
+		}
+	}
+};
+
+an.compositions = an.compositions || {};
+an.compositions['6B0F5B5223810D41B7F6ED92E9D2DD73'] = {
+	getStage: function() { return exportRoot.getStage(); },
+	getLibrary: function() { return lib; },
+	getSpriteSheet: function() { return ss; },
+	getImages: function() { return img; }
+};
+
+an.compositionLoaded = function(id) {
+	an.bootcompsLoaded.push(id);
+	for(var j=0; j<an.bootstrapListeners.length; j++) {
+		an.bootstrapListeners[j](id);
+	}
+}
+
+an.getComposition = function(id) {
+	return an.compositions[id];
+}
+
+
+// Layer depth API : 
+
+AdobeAn.Layer = new function() {
+	this.getLayerZDepth = function(timeline, layerName)
+	{
+		if(layerName === "Camera")
+		layerName = "___camera___instance";
+		var script = "if(timeline." + layerName + ") timeline." + layerName + ".depth; else 0;";
+		return eval(script);
+	}
+	this.setLayerZDepth = function(timeline, layerName, zDepth)
+	{
+		const MAX_zDepth = 10000;
+		const MIN_zDepth = -5000;
+		if(zDepth > MAX_zDepth)
+			zDepth = MAX_zDepth;
+		else if(zDepth < MIN_zDepth)
+			zDepth = MIN_zDepth;
+		if(layerName === "Camera")
+		layerName = "___camera___instance";
+		var script = "if(timeline." + layerName + ") timeline." + layerName + ".depth = " + zDepth + ";";
+		eval(script);
+	}
+	this.removeLayer = function(timeline, layerName)
+	{
+		if(layerName === "Camera")
+		layerName = "___camera___instance";
+		var script = "if(timeline." + layerName + ") timeline.removeChild(timeline." + layerName + ");";
+		eval(script);
+	}
+	this.addNewLayer = function(timeline, layerName, zDepth)
+	{
+		if(layerName === "Camera")
+		layerName = "___camera___instance";
+		zDepth = typeof zDepth !== 'undefined' ? zDepth : 0;
+		var layer = new createjs.MovieClip();
+		layer.name = layerName;
+		layer.depth = zDepth;
+		layer.layerIndex = 0;
+		timeline.addChild(layer);
+	}
+}
 
 
 })(createjs = createjs||{}, AdobeAn = AdobeAn||{});
